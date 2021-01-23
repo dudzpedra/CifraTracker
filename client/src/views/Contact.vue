0a0ea69659b496a8e152b743c600aa509e485b65
dudz <dudzpedra@outlook.com>
2021-01-23 18:19:11 -0300

    init vue cli

---
+++ client/src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="form">
+        <form>
+            <label for="name">Name: </label>
+            <input type="text" id="name">
+            <label for="email">E-mail: </label>
+            <input type="email" id="email">
+            <label for="msg">Message: </label>
+            <input type="text" id="msg">
+        </form>
+    </div>
+</template>
+
+<style scoped>
+#form {
+    color: rgb(129, 14, 120);
+    background-color: rgba(183, 228, 238, 0.867);
+}
+</style>
